d767af5c91eba5dfdcc47c996a90e86e1b7c781d
Yashasvi.Sagar <Yashasvi.Sagar@INC01007--.Incture.local>
2021-01-07 11:35:45 +0530

    commit

--- incubyte/src/com/core/incubyte/Main.java
+++ incubyte/src/com/core/incubyte/Main.java
@@ -3,9 +3,9 @@ package com.core.incubyte;
 public class Main {
 
 	public static void main(String[] args) {
-		StringCalculator sc = new StringCalculator();
-		System.out.println(sc.add("1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,11,11,1,1,1,2,3"));
-
+		System.out.println("Yash");
+		
+		System.out.println("Hello");
 	}
 
 }
